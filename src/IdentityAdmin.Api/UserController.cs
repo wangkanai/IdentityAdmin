42b1ff46319f37911e722810e044011e1471a3ec
Sarin Na Wangkanai <sarin@wangkanai.com>
2018-02-05 08:11:12 +0700

    testing all web api library

---
+++ src/IdentityAdmin.Api/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IdentityAdmin.Api
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await BuildUserViewModelAsync();
+            return View();
+        }
+
+        private async Task<UserViewModel> BuildUserViewModelAsync()
+        {
+            await Task.Delay(100);
+
+            return new UserViewModel { };
+        }
+    }
+
+    public class UserViewModel
+    {
+
+    }
+}
