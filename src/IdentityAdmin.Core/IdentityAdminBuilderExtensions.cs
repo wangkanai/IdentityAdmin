a5f45b2c04963418801556bd37aa7940932b8e31
Sarin Na Wangkanai <sarin@wangkanai.com>
2018-02-08 14:06:25 +0700

    refactoring UseIdentityAdmin()

---
+++ src/IdentityAdmin.Core/IdentityAdminBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2018 Sarin Na Wangkanai, All Rights Reserved.
+// The GNU GPLv3. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace IdentityAdmin
+{
+    public static class IdentityAdminBuilderExtensions
+    {
+        public static IApplicationBuilder UseIdentityAdmin(this IApplicationBuilder app)
+        {
+            if (app == null) throw new ArgumentNullException(nameof(app));
+
+            return app.UseMiddleware<IdentityAdminMiddleware>();
+        }
+
+        public static IApplicationBuilder UseIdentityAdmin(this IApplicationBuilder app, IdentityAdminOptions options)
+        {
+            if (app == null) throw new ArgumentNullException(nameof(app));
+            if (options == null) throw new ArgumentNullException(nameof(options));
+
+            return app.UseMiddleware<IdentityAdminMiddleware>(Options.Create(options));
+        }
+    }
+}
--- src/IdentityAdmin.Core/IdentityAdminService.cs
+++ src/IdentityAdmin.Core/IdentityAdminService.cs
@@ -1,10 +1,7 @@
 ﻿// Copyright (c) 2018 Sarin Na Wangkanai, All Rights Reserved.
 // The GNU GPLv3. See License.txt in the project root for license information.
 
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Options;
-using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -13,23 +10,5 @@ namespace IdentityAdmin
     public class IdentityAdminService : IIdentityAdminService
     {
         public HttpContext Context { get; }
-    }
-
-    public static class IdentityAdminBuilderExtensions
-    {
-        public static IApplicationBuilder UseIdentityAdmin(this IApplicationBuilder app)
-        {
-            if (app == null) throw new ArgumentNullException(nameof(app));
-
-            return app.UseMiddleware<IdentityAdminMiddleware>();
-        }
-
-        public static IApplicationBuilder UseIdentityAdmin(this IApplicationBuilder app, IdentityAdminOptions options)
-        {
-            if (app == null) throw new ArgumentNullException(nameof(app));
-            if (options == null) throw new ArgumentNullException(nameof(options));
-
-            return app.UseMiddleware<IdentityAdminMiddleware>(Options.Create(options));
-        }
     }
 }
