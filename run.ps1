aa3f5f87aa5bea042eb9e92d1c21c73ed7ca1301
Sarin Na Wangkanai <sarin@wangkanai.com>
2019-12-26 00:26:29 +0700

    fixed build error

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
 ./dotnet-install.ps1 -Channel Current -version 3.0.100
 dotnet --version
 dotnet restore
-dotnet build -c Release
+dotnet build
 dotnet test
 dotnet pack ./src/IdentityAdmin/src/IdentityAdmin.csproj -c Release -o .\artifacts
