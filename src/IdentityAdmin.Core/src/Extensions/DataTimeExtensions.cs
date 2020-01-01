825735ee749dd2637acfd965bf8febd50a9fd621
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-01-01 10:35:42 +0700

    add init extensions to core

---
+++ src/IdentityAdmin.Core/src/Extensions/DataTimeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Wangkanai.IdentityAdmin.Extensions
+{
+    public static class DataTimeExtensions
+    {
+        public static bool HasExpired(this DateTime? expirationTime, DateTime now)
+        {
+            if (expirationTime.HasValue && expirationTime.Value.HasExpired(now))
+                return true;
+
+            return false;
+        }
+
+        public static bool HasExpired(this DateTime expirationTime, DateTime now)
+        {
+            if (now > expirationTime)
+                return true;
+
+            return false;
+        }
+    }
+}
