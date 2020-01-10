2a997c670a5113736e980ad1a9358941fe85e28e
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-10 15:02:27 +0700

    init admin portal ui

---
+++ src/IdentityAdmin.UI/src/Areas/Admin/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Wangkanai.IdentityAdmin.UI.Admin.Pages.Internal
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/IdentityAdmin.UI/src/IdentityAdminBuilderUIExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wangkanai.IdentityAdmin
+{
+    public static class IdentityAdminBuilderUIExtensions
+    {
+        public static IdentityAdminBuilder AddDefaultUI(this IdentityAdminBuilder builder)
+        {
+            return builder;
+        }
+    }
+}
