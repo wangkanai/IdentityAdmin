9e07becc2a73752845008e83c262fd7bba44e993
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-10 11:47:39 +0700

    add default services for all interface concept

--- src/IdentityAdmin.Core/src/Services/Default/UserService.cs
+++ src/IdentityAdmin.Core/src/Services/Default/UserService.cs
@@ -1,7 +1,17 @@
-﻿namespace Wangkanai.IdentityAdmin.Services
-{
-    public class UserService : IUserService
-    {
+﻿// Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
+// The Apache v2. See License.txt in the project root for license information.
 
-    }
+namespace Wangkanai.IdentityAdmin.Services
+{
+    public class UserService : IUserService { }
+
+    public class ApiResourceService : IApiResourceService { }
+
+    public class ClaimService : IClaimServices { }
+
+    public class ClientService : IClientService { }
+
+    public class IdentityResource : IIdentityResourceService { }
+
+    public class RoleService : IRoleService { }
 }
--- src/IdentityAdmin.Core/src/Services/IClientService.cs
+++ src/IdentityAdmin.Core/src/Services/IClientService.cs
@@ -1,4 +1,7 @@
-﻿namespace Wangkanai.IdentityAdmin.Services
+﻿// Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
+// The Apache v2. See License.txt in the project root for license information.
+
+namespace Wangkanai.IdentityAdmin.Services
 {
     public interface IClientService { }
 }
