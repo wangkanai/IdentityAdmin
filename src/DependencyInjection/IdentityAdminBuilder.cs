491cc2b8e651ee63b7a902d4732ae9b4819e4126
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-02-05 17:16:44 +0700

    Refactor

--- src/DependencyInjection/IdentityAdminBuilder.cs
+++ src/DependencyInjection/IdentityAdminBuilder.cs
@@ -1,4 +1,4 @@
-﻿﻿// Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
+﻿// Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
 // The Apache v2. See License.txt in the project root for license information.
 
 using System;
@@ -7,22 +7,39 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public class IdentityAdminBuilder : IIdentityAdminBuilder
     {
-        public IServiceCollection Services { get; }
-        
-        public Type UserType { get; }
-
-        public Type RoleType { get; }
-
+        /// <summary>
+        /// Creates a new instanace of <see cref="IdentityAdminBuilder"/>
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="services">The <see cref="IServiceCollection" /> to attach to.</param>
         public IdentityAdminBuilder(Type user, IServiceCollection services)
         {
             Services = services ?? throw new ArgumentNullException(nameof(services));
             UserType = user ?? throw new ArgumentNullException(nameof(user));
         }
 
-        public IdentityAdminBuilder(Type user, Type role, IServiceCollection services) 
+        /// <summary>
+        /// Creates a new instanace of <see cref="IdentityAdminBuilder"/>
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="role"></param>
+        /// <param name="services">The <see cref="IServiceCollection" /> to attach to.</param>
+        public IdentityAdminBuilder(Type user, Type role, IServiceCollection services)
             : this(user, services)
         {
             RoleType = role ?? throw new ArgumentNullException(nameof(role));
         }
+
+        public Type UserType { get; }
+
+        public Type RoleType { get; }
+
+        /// <summary>
+        /// Gets the <see cref="IServiceCollection" /> services are attached to.
+        /// </summary>
+        /// <value>
+        /// The <see cref="IServiceCollection" /> services are attached to.
+        /// </value>
+        public IServiceCollection Services { get; }
     }
 }
