f89f8d2c0dd0b905d4c0a58a04a4be069eb3e671
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-02-05 14:55:55 +0700

    move to src folder

---
+++ src/DependencyInjection/IdentityAdminBuilder.cs
@@ -0,0 +1,18 @@
+﻿﻿// Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
+// The Apache v2. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public class IdentityAdminBuilder : IIdentityAdminBuilder
+    {
+        public IServiceCollection Services { get; private set; }
+
+        public IdentityAdminBuilder(IServiceCollection services)
+        {
+            Services = services
+                ?? throw new ArgumentNullException(nameof(services));
+        }
+    }
+}
