20887b96c89f6197f7f18f5427e9361efda862e2
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-02-09 15:23:03 +0700

    Mock Refactoring

--- test/Mock/MockService.cs
+++ test/Mock/MockService.cs
@@ -1,7 +1,23 @@
-﻿namespace Wangkanai.IdentityAdmin
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Wangkanai.IdentityAdmin
 {
     internal static class MockService
     {
         
+        
+        #region Internal
+
+        private static IHttpContextAccessor HttpContextAccessor()
+            => new HttpContextAccessor {HttpContext = CreateContext()};
+
+        private static HttpContext CreateContext()
+        {
+            var context = new DefaultHttpContext();
+            context.Request.Headers.Add("User-Agent", new[] {"MockClient"});
+            return context;
+        }
+
+        #endregion
     }
 }
